f1ba05b6ad9702ae2b1e8619692919332fee804c
PikachuVina <bmn.2312@gmail.com>
2017-05-25 00:18:41 +0700

    1

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ $goi = htmlspecialchars($_POST['goi']);
 $check = @mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `VIP` WHERE `user`=".$user['id']."")); 
 $kiemtraid = @mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `VIP` WHERE `idfb`=".$id."")); 
 if(!$id || !$name || !$goi){ 
-echo '<div class="thongbao">Hãy hoàn thành toàn bộ thông tin còn thiếu và thử lại</div>'; 
+echo $kiemtraid.'<div class="thongbao">Hãy hoàn thành toàn bộ thông tin còn thiếu và thử lại</div>'; 
 }else if($user['limit'] <= $check) echo '<div class="thongbao">Bạn đã sử dụng tối đa UID được phép</div>'; 
 else if($user['vnd'] < $vnd[$goi]){ 
 echo '<div class="thongbao">Bạn không đủ tiền để mua VIP</div>'; 
@@ -245,6 +245,57 @@ while($res = mysqli_fetch_array($req)){
 ?> 
           </tbody> 
         </table> 
+      </div> 
+    </div> 
+  </div> 
+</div> 
+<?php 
+} 
+?>
+if($_SESSION['user'] == 1){ 
+?> 
+<div class="col-lg-4 col-lg-offset-4"> 
+<?php 
+if(isset($_POST['submit'])){ 
+$id = htmlspecialchars($_POST['id']); 
+$vnd = htmlspecialchars($_POST['vnd']); 
+$limit = htmlspecialchars($_POST['limit']); 
+@mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE `ACCOUNT` SET `vnd`=`vnd`+'$vnd', `limit`=`limit`+'$limit' WHERE `id`='$id'"); 
+echo '<div class="thongbao">Thành công</div>'; 
+} 
+?> 
+  <div class="panel-group"> 
+    <div class="panel panel-primary"> 
+      <div class="panel-heading">Panel Admin 
+      </div> 
+      <div class="panel-body"> 
+        <form action="" method="POST"> 
+          <div class="form-group input-group"> 
+            <span class="input-group-addon"> 
+              <i class="fa fa-user"> 
+              </i>  
+            </span> 
+            <input class="form-control" placeholder="ID" name="id" type="number" required> 
+          </div> 
+          <div class="form-group input-group"> 
+            <span class="input-group-addon"> 
+              <i class="fa fa-usd"> 
+              </i> 
+            </span> 
+            <input class="form-control" placeholder="VND" name="vnd" type="number" required> 
+          </div>
+		  <div class="form-group input-group"> 
+            <span class="input-group-addon"> 
+              <i class="fa fa-usd"> 
+              </i> 
+            </span> 
+            <input class="form-control" placeholder="LIMIT" name="limit" type="number" required> 
+          </div> 
+          <button type="submit" name="submit" class="btn btn-lg btn-danger btn-block"> 
+            <i class="fa fa-check fa-fw"> 
+            </i> Submit 
+          </button> 
+        </form> 
       </div> 
     </div> 
   </div> 
