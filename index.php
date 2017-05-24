262aa67f13b56170fb6f846534efa21baef7b2e6
PikachuVina <bmn.2312@gmail.com>
2017-05-24 23:48:55 +0700

    1

--- index.php
+++ index.php
@@ -106,10 +106,7 @@ if(isset($_POST['add'])){
 $id = htmlspecialchars($_POST['id']); 
 $name = htmlspecialchars($_POST['name']); 
 $goi = htmlspecialchars($_POST['goi']); 
-$result= @mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `VIP` WHERE `user` = $user['id']");
-$taikhoan = @mysqli_fetch_array($result, MYSQLI_ASSOC);
-$check = $taikhoan;
-//$check = @mysqli_fetch_array(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `VIP` WHERE `user`=".$user['id']." ORDER BY RAND()")); 
+$check = @mysqli_fetch_array(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT COUNT(*) FROM `VIP` WHERE `user`=".$user['id']."")); 
 if(!$id || !$name || !$goi){ 
 echo '<div class="thongbao">Hãy hoàn thành toàn bộ thông tin còn thiếu và thử lại</div>'; 
 }else if($user['limit'] < $check) echo $check.'<div class="thongbao">Bạn đã sử dụng tối đa UID được phép</div>'; 
