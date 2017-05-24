572c3391f9291faa53dedab8020c168517a5db5d
PikachuVina <bmn.2312@gmail.com>
2017-05-24 22:53:15 +0700

    1

---
+++ naptien.php
@@ -0,0 +1,56 @@
+<?php
+include './include/head.php';
+?>
+<div class="col-lg-8 col-lg-offset-2">
+  <div class="panel panel-primary">
+    <div class="panel-heading">
+      <i class="fa fa-star-half-o fa-spin">
+      </i> NẠP BẰNG THẺ CÀO ĐIỆN THOẠI • PHÍ: 20%
+    </div>
+    <div class="panel-body">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Liên Hệ</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Vui lòng liên hệ ADMIN để nạp bằng thẻ cào</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-8 col-lg-offset-2">
+  <div class="panel panel-primary">
+    <div class="panel-heading">
+      <i class="fa fa-star-half-o fa-spin">
+      </i> NẠP BẰNG PHƯƠNG THỨC KHÁC • PHÍ: 0%
+    </div>
+    <div class="panel-body">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>ĐỊA CHỈ WEB</th>
+            <th>EMAIL</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><a href="https://megacard.vn/">MEGACARD</a></td>
+            <td>BMN.2312@Gmail.Com</td>
+          </tr>
+		  <tr>
+            <td><a href="https://www.baokim.vn/">BẢO KIM</a></td>
+            <td>BMN.2312@Gmail.Com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div>
+<?php
+include './include/foot.php';
