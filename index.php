f695c8ebabaec9abf4a84dbdb9fc2c7998c0f075
PikachuVina <bmn.2312@gmail.com>
2017-05-25 00:46:13 +0700

    1

--- index.php
+++ index.php
@@ -109,11 +109,17 @@ $goi = htmlspecialchars($_POST['goi']);
 $check = @mysqli_num_rows(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `VIP` WHERE `user`=".$user['id']."")); 
 $kiemtraid = @mysqli_fetch_array(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT `idfb` FROM `VIP` WHERE `idfb`=".$id."")); 
 if(!$id || !$name || !$goi){ 
-echo $kiemtraid.'<div class="thongbao">Hãy hoàn thành toàn bộ thông tin còn thiếu và thử lại</div>'; 
+echo $kiemtraid['idfb'].'<div class="thongbao">Hãy hoàn thành toàn bộ thông tin còn thiếu và thử lại</div>'; 
 }
-else if($user['limit'] <= $check) echo '<div class="thongbao">Bạn đã sử dụng tối đa UID được phép</div>'; 
-else if($user['vnd'] < $vnd[$goi]){ echo '<div class="thongbao">Bạn không đủ tiền để mua VIP</div>'; }
-else if($id == $kiemtraid['idfb']){ echo '<div class="thongbao">UID đã tồn tại trên hệ thống</div>'; }
+else if($user['limit'] <= $check){ 
+	echo '<div class="thongbao">Bạn đã sử dụng tối đa UID được phép</div>'; 
+		}
+else if($user['vnd'] < $vnd[$goi]){
+	echo '<div class="thongbao">Bạn không đủ tiền để mua VIP</div>'; 
+		}
+else if($id == $kiemtraid['idfb']){
+	echo '<div class="thongbao">UID đã tồn tại trên hệ thống</div>'; 
+		}
 else{ 
 @mysqli_query($GLOBALS["___mysqli_ston"], "UPDATE `ACCOUNT` SET `vnd`=`vnd`-'$vnd[$goi]' WHERE `id`=".$user['id'].""); 
 $time = time()+30*24*3600; 
