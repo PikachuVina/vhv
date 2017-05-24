09a765d5ac4e66076a813978668079625e0fda75
PikachuVina <bmn.2312@gmail.com>
2017-05-24 23:02:10 +0700

    1

--- naptien.php
+++ naptien.php
@@ -51,10 +51,43 @@ include './include/head.php';
           </tr>
         </tbody>
       </table>
-</br>• Nội dung: <font color="red">CTV - TTK</font>
+• Nội dung: <font color="red">CTV - TTK</font>
 • Trong đó: </br>
 - CTV: là cú pháp của web. </br>
-- TTK: là tên tài khoản của bạn. </br>
+- TTK: thay bằng tên tài khoản của bạn. </br>
+• Sau khi nạp, vui lòng liên hệ và báo với ADMIN để được cộng tiền vào tài khoản cho bạn trên trang web.</br>
+    </div>
+  </div>
+</div>
+
+<div class="col-lg-8 col-lg-offset-2">
+  <div class="panel panel-primary">
+    <div class="panel-heading">
+      <i class="fa fa-star-half-o fa-spin">
+      </i> NẠP TIỀN BẰNG NGÂN HÀNG • PHÍ: 0%
+    </div>
+    <div class="panel-body">
+• Nạp tiền vào trang web qua thẻ ATM các ngân hàng hoặc chuyển khoản trực tuyến bằng mobile banking, internet Banking và nhận được 100% giá trị tiền mặt vào tài khoản sau khi nạp.</br>
+• Thông tin chuyển khoản:</br></br>
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>NGÂN HÀNG</th>
+            <th>SỐ TÀI KHOẢN</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Chưa có nội dung</td>
+            <td>Chưa có nội dung</td>
+          </tr>
+        </tbody>
+      </table>
+• Người thụ hưởng: BUI MANH NGHIA.</br>
+• Nội dung: CTV - TTK</br>
+• Trong đó: </br>
+- CTV: là cú pháp của web. </br>
+- TTK: thay bằng tên tài khoản của bạn.</br> 
 • Sau khi nạp, vui lòng liên hệ và báo với ADMIN để được cộng tiền vào tài khoản cho bạn trên trang web.</br>
     </div>
   </div>
