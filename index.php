4c66dbceb94f67b0ae42136cb70597c420dde967
PikachuVina <bmn.2312@gmail.com>
2017-05-24 23:24:47 +0700

    1

--- index.php
+++ index.php
@@ -106,10 +106,10 @@ if(isset($_POST['add'])){
 $id = htmlspecialchars($_POST['id']); 
 $name = htmlspecialchars($_POST['name']); 
 $goi = htmlspecialchars($_POST['goi']); 
-$check = @mysqli_fetch_array(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `VIP` WHERE `user`=".$user['id']."")); 
+$check = @mysqli_fetch_array(mysqli_query($GLOBALS["___mysqli_ston"], "SELECT * FROM `VIP` WHERE `user`=".$user['id']." ORDER BY RAND()")); 
 if(!$id || !$name || !$goi){ 
-echo '<div class="thongbao">Please fill form fully</div>'; 
-}else if($user['limit'] < $check) echo '<div class="thongbao">Bạn đã sử dụng tối đa ID được phép</div>'; 
+echo '<div class="thongbao">Hãy hoàn thành toàn bộ thông tin còn thiếu và thử lại</div>'; 
+}else if($user['limit'] < $check) echo $check.'<div class="thongbao">Bạn đã sử dụng tối đa UID được phép</div>'; 
 else if($user['vnd'] < $vnd[$goi]){ 
 echo '<div class="thongbao">Bạn không đủ tiền để mua VIP</div>'; 
 }else{ 
