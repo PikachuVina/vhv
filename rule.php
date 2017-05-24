a02d64917ba3fd0bf7b2c08fd4653fe5763c110e
PikachuVina <bmn.2312@gmail.com>
2017-05-24 22:19:13 +0700

    1

---
+++ rule.php
@@ -0,0 +1,22 @@
+<?php
+include './include/head.php';
+?>
+<div class="col-lg-8 col-lg-offset-2">
+  <div class="panel panel-primary">
+    <div class="panel-heading">
+      <i class="fa fa-thumbs-o-up">
+      </i> Quy Định Sử Dụng
+    </div>
+    <div class="panel-body">
+      + Do rất nhiều ID chạy trong hệ thống vào giờ cao điểm nên ID bạn cài sẽ được nhận vào hệ thống trong vòng từ 0-60 phút. </br>
+      + Thời gian tốt nhất để cài và nhận được nhanh là từ 8h tối cho đến 11h sáng! </br>
+      + Hệ thống sẽ lên like trong vòng 0-20p sau khi post bài.</br>
+      + Khi lên like, hệ thống sẽ chọn bài mới nhất để lên. Nghĩa là muốn post liên tiếp mà lên đủ, bạn phải post và chờ hệ thống bắt đầu lên like rồi hãy post tiếp.</br>
+      + Album có thể sẽ ko lên like nếu trùng tên, nên post album có tên khác nhau.</br>
+      + Vui lòng liên hệ <a href="http://facebook.com/bmn.2312">Admin</a> Nếu như bạn gặp lỗi, nếu có trường hợp khiêu khích hoặc muốn up phốt ba cái nhảm nhỉ thì sẽ lập tức bị band khỏi hệ thống.</br>
+      Nếu có thắc mắc về vấn đề nạp tiền hay cách sử dụng hệ thống. Xin vui lòng liên hệ <a href="http://facebook.com/bmn.2312">Admin</a></br>
+    </div>
+  </div>
+</div>
+<?php
+include './include/foot.php';
