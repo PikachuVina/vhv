6a48275aa8003ef187c300cb0d2dab8573566a63
PikachuVina <bmn.2312@gmail.com>
2017-05-26 10:10:16 +0700

    1

--- dangky.php
+++ dangky.php
@@ -10,7 +10,7 @@ $username = htmlspecialchars($_POST['username']);
 $password = htmlspecialchars($_POST['password']); 
 $captcha = htmlspecialchars($_POST['captcha']); 
 $captcha_number = htmlspecialchars($_POST['captcha_number']); 
-$check = @mysqli_fetch_array(@mysqli_query($GLOBALS["___BMN_2312"], "SELECT COUNT(*) FROM `ACCOUNT` WHERE `username`='$username'"),  0); 
+$check = @mysqli_fetch_array(@mysqli_query($GLOBALS["___BMN_2312"], "SELECT COUNT(*) FROM `ACCOUNT` WHERE `username`='$username'")); 
 if(!$username || !$password || $captcha != $captcha_number){ 
 echo '<div class="thongbao">Hãy hoàn thành toàn bộ thông tin</div>'; 
 }else if($check > 0){ 
