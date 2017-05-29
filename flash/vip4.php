17a5b9a3bc92e25dcbfdee67941d618c97378627
PikachuVina <bmn.2312@gmail.com>
2017-05-29 15:21:52 +0700

    1

--- flash/vip4.php
+++ flash/vip4.php
@@ -3,7 +3,7 @@ error_reporting(0);
 set_time_limit(0);  
 include '../include/config.php'; 
 $resid = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM `vip` WHERE `goi` = 4"); 
-$restk = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM token ORDER BY RAND() LIMIT 0,120"); 
+$restk = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM token ORDER BY RAND() LIMIT 0,100"); 
 while ($vipid = @mysqli_fetch_array($resid)){ 
     $idfb = $vipid['idfb']; 
     while ($viptk = @mysqli_fetch_array($restk)) { 
@@ -12,9 +12,9 @@ while ($vipid = @mysqli_fetch_array($resid)){
         if (($vipid['time'] - time()) <= 0) { 
             @mysqli_query($GLOBALS["___BMN_2312"], "DELETE FROM `vip` WHERE `idfb`='".$vipid['idfb']."' "); 
         } 
-        $stat = json_decode(auto('https://graph.facebook.com/'.$idfb.'/feed?fields=id&access_token='.$token.'&limit=1'),true); 
+        $stat = json_decode(auto('https://graph.facebook.com/'.$idfb.'/feed??fields=id,likes&access_token='.$token.'&limit=1'),true); 
         $countlike = $stat[data][0][likes][count]; 
-        if($countlike <= 40){ 
+        if($countlike <= 690){ 
             for($i=1;$i<=count($stat['data']);$i++){ 
                 auto('https://graph.facebook.com/'.$stat['data'][$i-1]['id'].'/likes?access_token='.$token.'&method=post'); 
             } 
