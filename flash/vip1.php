4df8e79398d19b2fd6dcf0978a16221d0f338b8a
PikachuVina <bmn.2312@gmail.com>
2017-05-29 14:07:00 +0700

    1

--- flash/vip1.php
+++ flash/vip1.php
@@ -1,6 +1,7 @@
 <?php  
 error_reporting(0);
-set_time_limit(0);  
+set_time_limit(0);
+$like = array(0, 150, 300, 500, 700, 1000, 1500, 2000, 3000, 4000, 5000);   
 include '../include/config.php'; 
 $resid = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM `vip` WHERE `goi` = 1"); 
 $restk = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM token ORDER BY RAND() LIMIT 0,60"); 
@@ -14,7 +15,7 @@ while ($vipid = @mysqli_fetch_array($resid)){
         } 
         $stat = json_decode(auto('https://graph.facebook.com/'.$idfb.'/feed?fields=id&access_token='.$token.'&limit=1'),true); 
         $countlike = $stat[data][0][likes][count]; 
-        if($countlike <= 150){ 
+        if($countlike <= $like[$vipid['goi']]){
             for($i=1;$i<=count($stat['data']);$i++){ 
                 auto('https://graph.facebook.com/'.$stat['data'][$i-1]['id'].'/likes?access_token='.$token.'&method=post'); 
             } 
@@ -33,5 +34,8 @@ function auto($url) {
    curl_close($ch); 
    return $result; 
 } 
+$ga = @mysqli_fetch_array($resid);
+$nghia = $ga['time'] - time();
+echo $nghia;
 echo 'Xong'; 
 ?> 
