20b239ecda5d5cccf3f4e589bbd372f0be3947a6
PikachuVina <bmn.2312@gmail.com>
2017-05-29 14:12:01 +0700

    1

--- flash/vip1.php
+++ flash/vip1.php
@@ -5,6 +5,18 @@ $like = array(0, 150, 300, 500, 700, 1000, 1500, 2000, 3000, 4000, 5000);
 include '../include/config.php'; 
 $resid = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM `vip` WHERE `goi` = 1"); 
 $restk = @mysqli_query($GLOBALS["___BMN_2312"], "SELECT * FROM token ORDER BY RAND() LIMIT 0,60"); 
+$vipid = @mysqli_fetch_array($resid);
+$stat = json_decode(auto('https://graph.facebook.com/'.$idfb.'/feed?fields=id&access_token='.$token.'&limit=1'),true); 
+$countlike = $stat[data][0][likes][count];
+if($countlike <= $like[$vipid['goi']]){
+echo "ok"; 
+        }
+else
+{
+	echo "no";
+}
+
+/*
 while ($vipid = @mysqli_fetch_array($resid)){ 
     $idfb = $vipid['idfb']; 
     while ($viptk = @mysqli_fetch_array($restk)) { 
@@ -22,6 +34,7 @@ while ($vipid = @mysqli_fetch_array($resid)){
         } 
     } 
 }
+*/
 function auto($url) { 
    $ch = curl_init(); 
    curl_setopt_array($ch, array( 
@@ -34,8 +47,6 @@ function auto($url) {
    curl_close($ch); 
    return $result; 
 } 
-$ga = @mysqli_fetch_array($resid);
-$nghia = $ga['time'] - time();
-echo $nghia;
+
 echo 'Xong'; 
 ?> 
